ba0720da92a08d87e567d5e78ade439ffe0c7cf1
Nayeli Fabiola Armendariz Martinez <victorarmendariz86@gmail.com>
2023-10-25 21:04:48 -0600

    cambio4

--- practicas/unidad2/java/persona.java
+++ practicas/unidad2/java/persona.java
@@ -16,25 +16,4 @@ class Persona {
         vive = false;
     }
 
-    public static void main(String[] args) {
-        Persona persona = new Persona();
-
-        persona.edad = 25;
-        persona.nombre = "Rogelio";
-        persona.vive = true; 
-
-        persona.saludar();
-        System.out.println("La edad de la persona es " + persona.edad);
-        System.out.println("la persona vive?" + persona.vive);
-
-        persona.incrementarEdad();
-        System.out.println("La edad de la persona es " + persona.edad);
-        System.out.println("la persona vive?" + persona.vive);
-
-        persona.morir();
-        
-        System.out.println("La edad de la persona es " + persona.edad);
-        System.out.println("la persona vive?" + persona.vive);
-
     }
-}
