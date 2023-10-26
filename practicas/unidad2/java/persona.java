7be0cfaaa81094b7f12f7527de854608878f9c06
Nayeli Fabiola Armendariz Martinez <victorarmendariz86@gmail.com>
2023-10-25 20:32:38 -0600

    cambios

---
+++ practicas/unidad2/java/persona.java
@@ -0,0 +1,40 @@
+class persona {
+    String nombre;
+    int edad;
+    boolean vive;
+
+
+
+    void saludar() {
+        System.out.println("Hola mi nombre es" + nombre);
+
+    }
+    void incrementarEdad() {
+        edad = + 1;
+    }
+    void morir() {
+        vive = false;
+    }
+
+    public static void main(Sting[] args) {
+        Persona persona = new Persona();
+
+        persona.edad = 25;
+        persona.nombre = "Rogelio";
+        persona.vive = true;
+
+        persona.saludar();
+        System.out.println("La edad de la persona es" + persona.edad);
+        System.out.println("la persona vive?" + persona.vive);
+
+        persona.incrementarEdad();
+        System.out.println("La edad de la persona es" + persona.edad);
+        System.out.println("la persona vive?" + persona.vive);
+
+        persona.morir();
+        
+        System.out.println("La edad de la persona es" + persona.edad);
+        System.out.println("la persona vive?" + persona.vive);
+
+    }
+}
